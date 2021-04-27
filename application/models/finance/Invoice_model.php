f3a2250906a0535366bfe61806385b67ba6ed06b
annur25 <hafidz.bdt@gmail.com>
2021-04-27 14:16:15 +0700

    Update 27-04-2021

--- application/models/finance/Invoice_model.php
+++ application/models/finance/Invoice_model.php
@@ -16,8 +16,6 @@ class Invoice_model extends CI_model
 
     public function showAllByProg($m, $y, $p) {
         $this->db->select('sum(tbl_inv.inv_totpridr) as tot');
-        $this->db->join('tbl_stprog', 'tbl_stprog.stprog_id=tbl_inv.stprog_id');
-        $this->db->join('tbl_prog', 'tbl_prog.prog_id=tbl_stprog.prog_id');
         $this->db->where('tbl_stprog.stprog_status', 1);
         $this->db->like('tbl_prog.prog_sub', $p);
         $this->db->group_start();
@@ -30,17 +28,27 @@ class Invoice_model extends CI_model
             $this->db->or_where("YEAR(tbl_stprog.stprog_ass_sent) =", $y);
             $this->db->or_where("YEAR(tbl_stprog.stprog_init_consult) =", $y);
         $this->db->group_end();
+        $this->db->join('tbl_stprog', 'tbl_stprog.stprog_id=tbl_inv.stprog_id');
+        $this->db->join('tbl_prog', 'tbl_prog.prog_id=tbl_stprog.prog_id');
         return $this->db->get('tbl_inv')->result_array();
     }
 
     public function showAllByProgMain($m, $y, $p) {
         $this->db->select('sum(tbl_inv.inv_totpridr) as tot');
+        $this->db->where('tbl_prog.prog_main', $p);
+        $this->db->where('tbl_stprog.stprog_status', 1);
+        $this->db->group_start();
+            $this->db->where("MONTH(tbl_stprog.stprog_statusprogdate) =", $m);
+            $this->db->or_where("MONTH(tbl_stprog.stprog_ass_sent) =", $m);
+            $this->db->or_where("MONTH(tbl_stprog.stprog_init_consult) =", $m);
+        $this->db->group_end();
+        $this->db->group_start();
+            $this->db->where("YEAR(tbl_stprog.stprog_statusprogdate) =", $y);
+            $this->db->or_where("YEAR(tbl_stprog.stprog_ass_sent) =", $y);
+            $this->db->or_where("YEAR(tbl_stprog.stprog_init_consult) =", $y);
+        $this->db->group_end();
         $this->db->join('tbl_stprog', 'tbl_stprog.stprog_id=tbl_inv.stprog_id');
         $this->db->join('tbl_prog', 'tbl_prog.prog_id=tbl_stprog.prog_id');
-        $this->db->where('tbl_stprog.stprog_status', 1);
-        $this->db->where('month(tbl_stprog.stprog_statusprogdate)', $m);
-        $this->db->where('year(tbl_stprog.stprog_statusprogdate)', $y);
-        $this->db->where('tbl_prog.prog_main', $p);
         return $this->db->get('tbl_inv')->result_array();
     }
 
