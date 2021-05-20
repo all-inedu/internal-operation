1b4c3195a4862e99a245f06680f1ffe5c622f9ed
annur25 <hafidz.bdt@gmail.com>
2021-05-20 14:50:29 +0700

    Update 20-05-2021

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,4 +1,8 @@
 <?php
+Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Api extends CI_Controller
